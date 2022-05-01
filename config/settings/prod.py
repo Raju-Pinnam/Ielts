c56ebfbaf48b43db44faf0c4ff8c27e674818bb7
Padmaraju Pinnam <padmaraju.pinnam@orchids.edu.in>
2022-05-01 09:56:41 +0530

    DJ DB URL

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,18 +1,20 @@
 from .base import *
+import dj_database_url
 
 print("Using Prod Settings")
 
 DEBUG = False
 ALLOWED_HOSTS = ['.heroku.com',]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ielts_db',
-        'USER': 'postgres',
-        'PASSWORD': 'raju@123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'LIVE_TEST_DB': True
-    }
-}
+DATABASES = {'default': dj_database_url.config()}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ielts_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'raju@123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#         'LIVE_TEST_DB': True
+#     }
+# }

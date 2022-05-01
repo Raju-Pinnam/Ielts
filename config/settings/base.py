8d501eded7c37df1c44250d9ea9b0d3361a61e72
Padmaraju Pinnam <padmaraju.pinnam@orchids.edu.in>
2022-05-01 08:49:56 +0530

    Removed Hard Code key

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,7 +15,7 @@ from decouple import config
 
 
 import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -48,8 +48,10 @@ INSTALLED_APPS = [
     'ielts_objectives',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,8 +122,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
+
+STATIC_ROOT = BASE_DIR / "root" / "static"
+MEDIA_ROOT = BASE_DIR / "root" / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
